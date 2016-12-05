eafa09c9f32983b1f6dc4a7f1472bce1c1363620
memsharded <james@conan.io>
2016-12-05 14:30:22 +0100

    basic example

---
+++ basic/conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile, tools
+import sys
+
+class HelloPythonConan(ConanFile):
+    name = "HelloPy"
+    version = "0.1"
+    exports = '*'
+    build_policy = "missing"
+
+    def package(self):
+        self.copy('*.py')
+
+    def package_info(self):
+        self.env_info.PYTHONPATH.append(self.package_folder)
